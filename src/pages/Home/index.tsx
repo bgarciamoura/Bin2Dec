6d42f2745849c143f3cdcd381e2203c06786277c
ptvitascience <64225622+ptvitascience@users.noreply.github.com>
2020-08-19 15:12:07 -0300

    Layout created and handleChange created

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
+
+import './style.css';
 
 const App = () => {
+    const [binaryValue, setBinaryValue] = useState(0);
+
+    function handleNumberChange(event: ChangeEvent<HTMLInputElement>) {
+        const value = parseInt(event.target.value);
+        setBinaryValue(value);
+    }
+
     return (
         <div className="container">
-            <h1>Oi, Bruno</h1>
+            <h1>Binary to Decimal</h1>
+            <div className="input-container">
+                <input
+                    type="text"
+                    min="0"
+                    value={binaryValue}
+                    onChange={handleNumberChange}
+                />
+            </div>
+            <div className="converted-value-container">
+                <span>{binaryValue}</span>
+            </div>
         </div>
     );
 };
